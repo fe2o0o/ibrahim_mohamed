0c63c439e37e2c0a045c1796198284e647d48260
ibrahim <ibrahimfe2o0o@gmail.com>
2024-10-20 08:42:07 +0300

    handle about me page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  {path:'home' , loadComponent:()=>HomePageComponent}
+  { path: 'home', loadComponent: ()=> import('./pages/home-page/home-page.component').then((m) => m.HomePageComponent) },
+  {path:"about" , loadComponent: ()=> import('./pages/about/about.component').then ((m) => m.AboutComponent)}
 ];
